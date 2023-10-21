7ca08795ace2038b43d717fac774b8876dc51a37
Maaz Khan <maazkhan7454@gmail.com>
2023-10-21 18:17:20 +0530

    added the city card & state in url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -6,8 +8,8 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import City from "./components/City";
 import CountryList from "./components/CountryList";
-import { useEffect, useState } from "react";
 
 
 const BASE_URL = "http://localhost:8000"; 
@@ -46,6 +48,7 @@ function App() {
         <Route path='app' element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path='cities/:id' element={<City />} />
           <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
