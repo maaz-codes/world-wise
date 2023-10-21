ae8bbaad17df023cbc6d6fdf3ffdb004111ac45c
Maaz Khan <maazkhan7454@gmail.com>
2023-10-21 16:51:52 +0530

    added countries section

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,13 +1,25 @@
-import Spinner from "./Spinner";
+import styles from './CountryList.module.css';
+import Spinner from './Spinner';
 import CountryItem from './CountryItem';
+import Message from './Message';
 
 function CountryList({ cities, isLoading }) {
 
     if(isLoading) return <Spinner />
 
+    if(!cities.length) return <Message message='Add your first city by clicking on a city on the map' />
+
+    // countries = {country: 'France', emoji: 'FR'}
+    const countries = cities.reduce((arr, city) => {
+        if(!arr.map((el) => el.country).includes(city.country))
+        return [...arr, {country: city.country, emoji: city.emoji}];
+        else return arr;
+    }, []);
+
     return (
-        <div>
-        </div>
+        <ul className={styles.countryList}>
+            {countries.map(country => <CountryItem country={country} key={Math.floor(Math.random()*10000)} />)}
+        </ul>
     )
 }
 
